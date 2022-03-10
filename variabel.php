7e3efe68f00058310dc0e35d100546ea79372043
arshell19 <arsheliaromadhona01@gmail.com>
2022-03-10 09:31:43 +0700

    menambahkan file 2

---
+++ variabel.php
@@ -0,0 +1,25 @@
+hasil penjumlahan dari 23 + 52 + 68 :
+<?php
+	$x = 23;
+	$y = 52;
+	$z = 68;
+	echo $x + $y + $z;
+?>
+<br>
+
+hasil pengurangan dari 203 - 154 - 7 :
+<?php
+	$a = 203;
+	$b = 154;
+	$c = 7;
+	echo $a - $b - $c;
+?>
+<br>
+
+hasil perkalian dari 25 x 4 x 5 :
+<?php
+	$d = 25;
+	$e = 4;
+	$f = 5;
+	echo $d * $e * $f;
+?>
